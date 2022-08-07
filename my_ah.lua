a5484a4a2bc65ded3f1dc75008d7df9b078cc421
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2022-08-07 17:43:19 +0200

    Update my_ah.lua

--- my_ah.lua
+++ my_ah.lua
@@ -1,6 +1,3 @@
-local ont = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()
-    end
-ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/Korabi-dev/roblox-crasher/main/my_ah.lua'),true))()")
 local path = "servers.txt"
 local isfile = isfile(path)
 local player = game.Players.LocalPlayer
